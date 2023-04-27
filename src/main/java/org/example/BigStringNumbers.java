9ef2f86bf6c8d4843521cf48ee0279b9a8da34ee
gb <gb@autanalabs.com>
2023-04-27 12:54:36 -0300

    implement getMaxLength

--- src/main/java/org/example/BigStringNumbers.java
+++ src/main/java/org/example/BigStringNumbers.java
@@ -1,11 +1,23 @@
 package org.example;
 
+import java.util.Arrays;
+import java.util.Comparator;
+
 public class BigStringNumbers {
 
     private BigStringNumbers() {}
 
     public static String add(String aNumber, String anotherNumber) {
+        final int maxLength = getMaxLength(aNumber, anotherNumber) + 1;
+        final byte[][] board = new byte[2][maxLength];
         return null;
+    }
+
+    public static int getMaxLength(String... numbers) {
+        return Arrays.stream(numbers)
+                .map(n ->  n.trim().length() )
+                .max(Comparator.naturalOrder())
+                .orElse(0);
     }
 
 }
--- src/test/java/org/example/BigStringNumbersTest.java
+++ src/test/java/org/example/BigStringNumbersTest.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigInteger;
@@ -9,13 +10,43 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class BigStringNumbersTest {
 
+    private Random rnd;
+
+    @BeforeEach
+    void setUp() {
+        rnd = new Random();
+    }
+
+    private BigInteger getRandomBigInteger() {
+        return getRandomBigInteger(rnd.nextInt(126) + 2);
+    }
+
+    private BigInteger getRandomBigInteger(int bitLength) {
+        return BigInteger.probablePrime(bitLength, rnd);
+    }
+
+    private int getBigIntegerSize(BigInteger number) {
+        return number.toString().trim().length();
+    }
+
     @Test
-    void WhenBothAddendsAreProvidedReturnTheSumOfThem() {
-        final Random rnd = new Random();
-        final BigInteger addend = BigInteger.probablePrime(128, rnd);
-        final BigInteger anotherAddend = BigInteger.probablePrime(128, rnd);
+    void GivenBothAddendsWhenAddReturnsTheSumOfThem() {
+
+        final BigInteger addend = getRandomBigInteger(128);
+        final BigInteger anotherAddend = getRandomBigInteger(64);
         final String expected = addend.add(anotherAddend).toString();
         final String actual = BigStringNumbers.add(addend.toString(), anotherAddend.toString());
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void GivenTwoStringsReturnsTheMaxLengthOfThem() {
+        final BigInteger first = getRandomBigInteger();
+        final int firstSize = getBigIntegerSize(first);
+        final BigInteger second = getRandomBigInteger();
+        final int secondSize = getBigIntegerSize(second);
+        final int expected = (firstSize >= secondSize)? firstSize : secondSize;
+        final int actual = BigStringNumbers.getMaxLength(first.toString(), second.toString());
         assertEquals(expected, actual);
     }
 }
